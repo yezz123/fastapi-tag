20b5c9c15a0635a85420e43c862b0aeef1998fe3
Yasser Tahiri <yasserth19@gmail.com>
2021-10-27 19:29:24 +0100

    chore: Create Tests

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from fastapi_tag import Application, Contact, Metadata, Version
+
+
+@pytest.fixture
+def metadata():
+    return Metadata(
+        title="<title>",
+        version=Version(app="v0.1.1", api="v0.1.0"),
+        description=None,
+        contact=Contact(name="name", url="http://test.com", email=None),
+        api_id="49786b4b-1889-46ec-bd72-27f332436e6f",
+        audience="company-internal",
+    )
+
+
+@pytest.fixture
+def app(metadata):
+    return Application("", metadata)
+
+
+def client(app):
+    return TestClient(app)
